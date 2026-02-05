51e15e7c179e4bbfd4c6cf8e226d6c1163c17d9b
bakakoozin <cyrilpebre@gmail.com>
2026-02-05 22:32:59 +0100

    authors

---
+++ src/Entity/Authors.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorsRepository;
+use BcMath\Number;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuthorsRepository::class)]
+class Authors
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 60)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $test = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
